9e47449f23ee4634289b9385cc0e3c84a5a0ad6a
Livia <hillary22@users.noreply.github.com>
2015-02-08 18:03:05 -0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+
+<head>
+
+<meta charset="iso-8859-1">
+<meta charset="utf-8">
+
+<title> Find 4 Me </title>
+
+<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="css/style-index.css">
+<link rel="stylesheet" type="text/css" href="css/estilo-comum.css"/>
+<script src="javascript/jquery.js"></script>
+<script charset="ISO-8859-1" type="text/javascript" src="javascript/funcoes.js"></script>
+</head>
+
+<body>
+
+	
+	<div class="page">
+    
+    	<header class="topo">
+        	<img class="logo" src="logo.JPG">            
+            <h1 class="alunos"> Universidade Federal da Paraíba </br> Centro de Informática </br> Arthur Chacon </br> Rafael Germano </h1>
+            <img class="logoufpb" src="logo_ufpb.JPG">
+        </header>
+    
+	    <div class="barra-menu">
+        	<nav class="barra">
+                <ul>
+                    <li><a class="buscarA" onClick="menuBuscar();">Home</a></span></li>
+                    <!--<li><a href="#info" class="informacao">Informações</a></li>
+                    <li><a href="#contact" class="contate-nos"><span>Contate-nos</span></a></li>-->
+                </ul>
+	        </nav>
+    	</div>
+        
+    	<div class="meio">
+        	<div class="buscarPainel">
+            </div>
+            <div class="resultadosPainel">
+            </div>
+        </div>
+        
+        
+    </div>
+
+</body>
+</html>
